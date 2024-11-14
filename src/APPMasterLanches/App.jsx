538d53653c44447a911d1f696d35d4f2559b01a1
thiago-Parreiras <thiagoparreiras25@gmail.com>
2024-11-14 20:45:04 -0300

    tela de bebidas

--- src/APPMasterLanches/App.jsx
+++ src/APPMasterLanches/App.jsx
@@ -6,6 +6,7 @@ import Login from './app/pages/login';
 import HomeScreen from './app/pages/telaPrincipal';
 import SearchScreen from './app/pages/search';
 import Cart from './app/pages/shoppingCart';
+import CategoriaBebidas from './app/pages/categorias/bebidas'
 
 const Stack = createNativeStackNavigator();
 export default function App() {
@@ -13,13 +14,14 @@ export default function App() {
         <CartProvider>
             <NavigationContainer>
                 <Stack.Navigator 
-                    initialRouteName="Home" 
+                    initialRouteName="Bebidas" 
                     screenOptions={{ animation: 'fade', headerShown: false }} 
                 >
                     <Stack.Screen name="Login" component={Login} />
                     <Stack.Screen name="Home" component={HomeScreen} />
                     <Stack.Screen name="Search" component={SearchScreen} />
                     <Stack.Screen name="Cart" component={Cart} />
+                    <Stack.Screen name="Bebidas" component={CategoriaBebidas} />
                 </Stack.Navigator>
             </NavigationContainer>
         </CartProvider>
